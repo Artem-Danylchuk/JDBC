20677bb0c5d414668c69e23a8359eaed136f98b5
Artem <129773344+Artem-Danylchuk@users.noreply.github.com>
2023-08-02 17:24:55 +0200

    Add files via upload

---
+++ JdbcDAO1/src/main/java/ua/kiev/prog/shared/Client.java
@@ -0,0 +1,60 @@
+package ua.kiev.prog.shared;
+
+public class Client {
+    @Id
+    private int id;
+
+    private String name;
+    private int age;
+    private long data;
+
+    public Client() {
+    }
+
+    public Client(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getData() {
+        return data;
+    }
+
+    public void setData(long data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", data=" + data +
+                '}';
+    }
+}
